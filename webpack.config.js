eccab1b1ddd41998adfe5dac6576819d3e6530c0
Baptiste Doucerain <doucerain.b@gmail.com>
2018-11-05 01:04:53 +0100

    Setup heroku server

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,36 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
-  },
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public")
-  }
+module.exports = env => {
+  const isProduction = env === "production" ? "production" : "development";
+  return {
+    mode: isProduction ? "prodcution" : "development",
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "styles.css"
+      })
+    ],
+    devtool: "cheap-module-eval-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public")
+    }
+  };
 };
